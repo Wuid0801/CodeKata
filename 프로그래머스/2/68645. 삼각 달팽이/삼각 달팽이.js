7b06e3ac7b8239d3a9f57e89c76357ac7034e9f3
강혜성 <105138020+Wuid0801@users.noreply.github.com>
2024-05-16 22:41:49 +0900

    [level 2] Title: 삼각 달팽이, Time: 29.84 ms, Memory: 101 MB -BaekjoonHub

---
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.js
@@ -0,0 +1,29 @@
+function solution(n) {
+    let triangle = Array.from({ length: n }, (_, i) => Array(i + 1).fill(0));
+
+    let num = 1;
+    let x = -1, y = 0;
+    
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            if (i % 3 === 0) { 
+                x++;
+            } else if (i % 3 === 1) { 
+                y++;
+            } else { 
+                x--;
+                y--;
+            }
+            triangle[x][y] = num++;
+        }
+    }
+
+    let result = [];
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j <= i; j++) {
+            result.push(triangle[i][j]);
+        }
+    }
+
+    return result;
+}
