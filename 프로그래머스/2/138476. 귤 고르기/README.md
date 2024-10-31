b3ca9224d29ab335c0eeae610483e69096e73eac
강혜성 <105138020+Wuid0801@users.noreply.github.com>
2024-10-31 22:47:21 +0900

    [level 2] Title: 귤 고르기, Time: 98.35 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/README.md
+++ 프로그래머스/2/138476. 귤 고르기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 귤 고르기 - 138476 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/138476?language=javascript) 
 
 ### 성능 요약
 
-메모리: 52.1 MB, 시간: 47.95 ms
+메모리: 66.4 MB, 시간: 98.35 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 22일 13:48:17
+2024년 10월 31일 22:47:09
 
 ### 문제 설명
 
--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,27 +1,22 @@
 function solution(k, tangerine) {
-    // 1) 귤의 크기별 개수를 담을 map 생성한다.
-    const kind = new Map();
-    let answer =0 ;
-    
-    // 2) 크기별 개수를 구한다.
-    tangerine.forEach(org => {
-        kind.set(org, kind.has(org) ? kind.get(org)+1 : 1);
-    });
+    // 귤 크기별로 개수 세기
+    const countMap = {};
+    for (const size of tangerine) {
+        countMap[size] = (countMap[size] || 0) + 1;
+    }
 
-    // 3) 귤의 개수에 따른 내림차순 정렬
-    const sortArr = [... kind].sort((a,b) => b[1] - a[1]);
+    // 크기별 개수를 내림차순 정렬
+    const counts = Object.values(countMap).sort((a, b) => b - a);
+
+    // 필요한 귤의 개수가 k개가 될 때까지 가장 개수가 많은 크기부터 선택
+    let selectedCount = 0;
+    let varietyCount = 0;
     
-    // 4) BOX에 담는다.
-    sortArr.forEach(arr => {
-        // 5) 담을 개수(k)가 0보다 클 때만 BOX에 담는다.
-        if(k > 0) {
-            // 6)  담을 개수 - 귤의 개수
-            k -= arr[1];    
-            
-            // 7) 담게 되면 Box에 새로운 크기의 귤이 들어 오므로 1증가
-            answer++;
-        } 
-    });
-    // 8) 귤의 크기별 종류를 return한다.
-     return answer;
-};
+    for (const count of counts) {
+        selectedCount += count;
+        varietyCount += 1;
+        if (selectedCount >= k) break;
+    }
+
+    return varietyCount;
+}
